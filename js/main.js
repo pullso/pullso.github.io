63033ed185bd26d4f8544901731c8f771e2ffd25
pullso <p.dubitskiy@ya.ru>
2020-04-15 19:34:09 +0500

    add setrs

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@ window.addEventListener("DOMContentLoaded", (e) => {
   const menuIcon = document.querySelector(".menu__icon");
   const linksBg = document.querySelector(".links__bg");
   const menuLinks = document.querySelector(".menu__links");
+  const tabs = document.querySelectorAll(".nav-item .nav-link");
+  const tabHtml = document.querySelectorAll(".category-html");
+  const tabVue = document.querySelectorAll(".category-vue");
+  const tabJs = document.querySelectorAll(".category-js");
+
+  console.log(tabs);
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log(e.target.text);
+      document.querySelector(".active_tab").classList.remove("active_tab");
+      e.target.classList.add('ative_tab');
+
+      // body
+    });
+  });
 
   menuIcon.addEventListener("click", (e) => {
     console.log("click");
