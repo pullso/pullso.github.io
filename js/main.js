4e7099627f132dbcc74bc05aa75c1ab9e26e6919
pullso <p.dubitskiy@ya.ru>
2020-04-15 21:13:22 +0500

    add some animation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
+new WOW().init();
+
 window.addEventListener("DOMContentLoaded", (e) => {
   const menuIcon = document.querySelector(".menu__icon");
   const linksBg = document.querySelector(".links__bg");
   const menuLinks = document.querySelector(".menu__links");
   const tabs = document.querySelectorAll(".nav-item .nav-link");
-  const tabHtml = document.querySelectorAll(".category-html");
-  const tabVue = document.querySelectorAll(".category-vue");
-  const tabJs = document.querySelectorAll(".category-js");
 
   console.log(tabs);
 
   tabs.forEach((tab) => {
     tab.addEventListener("click", (e) => {
       e.preventDefault();
-      console.log(e.target.text);
       document.querySelector(".active_tab").classList.remove("active_tab");
-      e.target.classList.add('ative_tab');
+      e.target.classList.add("active_tab");
+      const items = document.querySelectorAll(".portfolio  .col-xl-4");
+
+      items.forEach((item) => {
+        if (item.classList.contains(e.target.id) || e.target.id === "all") {
+          item.classList.remove("disable");
+          item.classList.toggle("active");
+        } else {
+          item.classList.remove("active");
+          item.classList.add("disable");
+        }
+      });
 
       // body
     });
