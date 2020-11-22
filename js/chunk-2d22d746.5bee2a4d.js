1d78b2a8f5fbcf5a2c4e318f604cb3eb9b1b4cfb
pullso <pullso@users.noreply.github.com>
2020-11-22 16:31:23 +0000

    deploy: 97f25772f66f886401147d1585ac56edfdb69ed8

---
+++ js/chunk-2d22d746.5bee2a4d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("this is about page")])])}],u=n("2877"),c={},i=Object(u["a"])(c,s,a,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.5bee2a4d.js.map
