7aca184f3473b8f98f719b16011e89b3e3e8777a
pullso <p.dubitskiy@ya.ru>
2020-11-22 18:59:46 +0300

    fix test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,8 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.find('h1').text()).toMatch('Welcome to Vuetify');
   });
 });
